8a40019df58d89cdca6d6f0462be9e2289a95cc4
anand-rathore <av44314@gmail.com>
2022-07-21 14:05:37 +0530

    added methods to kyc_view

---
+++ apis/api/kyc_view.py
@@ -0,0 +1,23 @@
+from rest_framework.decorators import api_view
+
+
+@api_view(['POST'])
+def save_general_info(request):
+    pass
+
+
+@api_view(['POST'])
+def save_business_info(request):
+    pass
+
+
+@api_view(['POST'])
+def save_merchant_info(request):
+    pass
+
+
+@api_view(['POST'])
+def save_settlement_info(request):
+    pass
+
+
