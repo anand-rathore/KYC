ccdfec74b1750dfc08dca2303bf4f940666ea2c9
sonu <sonu@gmail.com>
2022-07-26 16:31:48 +0530

    create APIs/ URL update

--- apis/urls.py
+++ apis/urls.py
@@ -6,8 +6,8 @@ from rest_framework import routers
 from apis.api.GetMerchantData import GetMerchantData, GetMerchantDocumentByID
 from apis.api.document_type_master_view import DocumentTypeMasterAPI
 from apis.api.merchant_document_view import MerchantDocumentAPI, get_merchant_doc
-from apis.api.kyc_view import save_general_info_api, save_merchant_info_api
 from apis.databaseService.lookup_service import get_lookup_state
+from apis.api.kyc_view import save_business_info_api, save_Settlement_InfoOther, save_general_info_api, save_merchant_info_api
 
 
 router = routers.DefaultRouter()
@@ -16,11 +16,13 @@ router.register(r'upload-merchant-document', MerchantDocumentAPI, basename='uplo
 
 urlpatterns = [
     path('getMerchantData/', GetMerchantData.as_view()),
-    path('GetMerchantDocumentByID/<int:Id>', GetMerchantDocumentByID.as_view()),
+    path('GetMerchantDocumentByID/<int:Id>', GetMerchantDocumentByID.as_viewc()),
     path('get-merchant-document/', get_merchant_doc),
     path('', include(router.urls)),
-    path('savegeneralinfo/', save_general_info_api),   
+    path('savegeneralinfo/', save_general_info_api),
     path('savemerchantinfo/', save_merchant_info_api),
     path('getlookupstate/', get_lookup_state),
+    path('savebusinessinfo/', save_business_info_api), 
+    path('saveSettlementInfoOther/', save_Settlement_InfoOther)
 ]
 
